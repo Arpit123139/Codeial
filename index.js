fa9fa3bb32f8b1c94c2894953d98bbbc8cc51917
Arpit123139 <arpitgajya2001@gmail.com>
2022-09-26 21:46:18 +0530

    Setting up the View Engine

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port=8000
 /***********************use express router*************/
 app.use('/',require("./routes"))          // by doing this we are telling the app that all .get .post will be handle by this folder 
 
+/*****************Telling that we have to use ejs as View Engine */
+app.set('view engine','ejs')
+app.set('views','./views')
 app.listen(port,function(err){
     if(err){
         //Below both the statement are same 
