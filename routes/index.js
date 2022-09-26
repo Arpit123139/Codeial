604108b5568255f30d1e8d045b0e3a453604e3e6
Arpit123139 <arpitgajya2001@gmail.com>
2022-09-26 21:33:09 +0530

    adding multiple routers and controllers

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,7 @@ const homeController=require("../controllers/home_controller")
 // As we export the home so we are accessing it from home_controller.'js
 router.get('/',homeController.home )
 router.get('/temp',homeController.temp)
+
+/**************************This line says that when the request come something like /users then send it to the neighbour users.js  We have to do this because index.js(main) refers to this file so this file must have a list of all other route */
+router.use('/users',require("./users"))
 module.exports=router
